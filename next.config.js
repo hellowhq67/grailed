58eb0172aa61c77f01401d1fdafc61cb3c590ec3
Lijan Haque <93515488+hellowhq67@users.noreply.github.com>
2024-03-28 11:35:38 +0600

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -7,7 +7,7 @@ const nextConfig = {
         headers: [
           {
             key: "Access-Control-Allow-Origin",
-            value: "https://administration-tau.vercel.app", // Set your frontend origin
+            value: "https://administration-tau.vercel.app/", // Set your frontend origin
           },
           {
             key: "Access-Control-Allow-Methods",
